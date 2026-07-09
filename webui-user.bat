42780602f29043538cf279981ee2270cc397c906
ExtremeSumo <extremesumo163@gmail.com>
2026-07-09 22:29:28 +0400

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -4,7 +4,7 @@
 :: set GIT=
 :: set VENV_DIR=
 
-set COMMANDLINE_ARGS= --uv --nunchaku --bnb --cuda-malloc --onnxruntime-gpu 
+set COMMANDLINE_ARGS= --uv --nunchaku --bnb --cuda-malloc --onnxruntime-gpu
 
 :: --xformers --sage --uv
 :: --pin-shared-memory --cuda-malloc --cuda-stream
